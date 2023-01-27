0e26b36a4383cc440c1f189ccae27d01d381bdc6
marika <ms.czerwonka@gmail.com>
2023-01-27 22:23:27 +0100

    Add main logic

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Grid, Stack, Typography } from "@mui/material";
 
 const Main = () => {
-  return <div>Main</div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const dataFetch = async () => {
+      const url = "https://reqres.in/api/products";
+      try {
+        const result = await (await fetch(url)).json();
+        result.data.forEach((product) =>
+          setProducts((products) => [...products, product])
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    dataFetch();
+  }, []);
+
+  return (
+    <Grid item>
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={20}
+      >
+        <Typography variant="body2" gutterBottom color="textPrimary">
+          name
+        </Typography>
+        <Typography variant="body2" gutterBottom color="textPrimary">
+          color
+        </Typography>
+        <Typography variant="body2" gutterBottom color="textPrimary">
+          year
+        </Typography>
+        <Typography variant="body2" gutterBottom color="textPrimary">
+          pantone
+        </Typography>
+      </Stack>
+      {products.map((product, index) => {
+        return (
+          <Stack
+            key={index}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            spacing={20}
+          >
+            <Typography variant="body2" gutterBottom color="textSecondary">
+              {product.name}
+            </Typography>
+            <Typography
+              variant="body2"
+              gutterBottom
+              color="textSecondary"
+              sx={{ backgroundColor: product.color }}
+            >
+              {product.color}
+            </Typography>
+            <Typography variant="body2" gutterBottom color="textSecondary">
+              {product.year}
+            </Typography>
+            <Typography variant="body2" gutterBottom color="textSecondary">
+              {product.pantone_value}
+            </Typography>
+          </Stack>
+        );
+      })}
+    </Grid>
+  );
 };
 
 export default Main;
