c2dfb2db2d9f910379e9989abf04fb92a2a594ae
marika <ms.czerwonka@gmail.com>
2023-01-28 00:53:53 +0100

    Create searchbar

--- src/components/main/searchbar/Searchbar.js
+++ src/components/main/searchbar/Searchbar.js
@@ -1,7 +1,33 @@
 import React from "react";
+import styled from "styled-components";
 
-const Searchbar = () => {
-  return <div>Searchbar</div>;
+import { AppBar, Toolbar, InputBase } from "@mui/material";
+
+const CustomToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "center",
+});
+
+const CustomInputBase = styled(InputBase)({
+  padding: "5px",
+  border: "1px solid #3b3a30",
+  borderRadius: "5px",
+  width: "50%",
+  backgroundColor: "#b2c2bf",
+});
+
+const Searchbar = ({ setSearchedProduct }) => {
+  return (
+    <AppBar position="static">
+      <CustomToolbar>
+        <CustomInputBase
+          placeholder="Search"
+          type="number"
+          onChange={(event) => setSearchedProduct(event.target.valueAsNumber)}
+        />
+      </CustomToolbar>
+    </AppBar>
+  );
 };
 
 export default Searchbar;
