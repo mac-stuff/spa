269d543d6229cff5a07e3fc1517ab52d081dd13a
marika <ms.czerwonka@gmail.com>
2023-01-27 15:23:29 +0100

    Create foterbar

--- src/components/main/footerbar/Foterbar.js
+++ src/components/main/footerbar/Foterbar.js
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+import { AppBar, Typography } from "@mui/material";
+
+const CustomToolbar = styled(AppBar)({
+  textAlign: "center",
+  alignItems: "center",
+  padding: "20px",
+  borderRadius: "5px",
+});
 
 const Foterbar = () => {
-  return <div>Foterbar</div>;
+  return (
+    <CustomToolbar position="sticky">
+      <Typography>2023</Typography>
+    </CustomToolbar>
+  );
 };
 
 export default Foterbar;
