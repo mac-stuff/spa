f92c97bf35e8e550b40807bdc60d091ac88394f0
marika <ms.czerwonka@gmail.com>
2023-01-28 16:04:10 +0100

    Create pagination

---
+++ src/components/main/BasicPagination.js
@@ -0,0 +1,28 @@
+import { Button, Stack } from "@mui/material/";
+
+const BasicPagination = ({
+  startIndex,
+  setStartIndex,
+  endIndex,
+  setEndIndex,
+}) => {
+  const handleClickPrev = () => {
+    setStartIndex(startIndex - 5);
+    setEndIndex(endIndex - 5);
+  };
+
+  const handleClickNext = () => {
+    setStartIndex(startIndex + 5);
+    setEndIndex(endIndex + 5);
+  };
+  return (
+    <Stack spacing={2} direction="row">
+      <Button onClick={handleClickPrev} disabled={startIndex === 0}>
+        prev
+      </Button>
+      <Button onClick={handleClickNext}>next</Button>
+    </Stack>
+  );
+};
+
+export default BasicPagination;
